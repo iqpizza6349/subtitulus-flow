6df36cda3a4adbfd81758f443c03afd585094694
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-27 14:12:39 +0900

    :lipstick: :: create simple frame to show on window

--- src/main/java/io/iqpizza6349/subflow/Main.java
+++ src/main/java/io/iqpizza6349/subflow/Main.java
@@ -1,4 +1,10 @@
 package io.iqpizza6349.subflow;
 
+import io.iqpizza6349.subflow.window.MainWindow;
+
 public class Main {
+
+    public static void main(String[] args) {
+        new MainWindow().launchApplication(args);
+    }
 }
--- src/main/java/io/iqpizza6349/subflow/window/MainWindow.java
+++ src/main/java/io/iqpizza6349/subflow/window/MainWindow.java
@@ -1,4 +1,35 @@
 package io.iqpizza6349.subflow.window;
 
-public class MainWindow {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+
+    public void launchApplication(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("SubtitulusFlow");
+
+        // Create the root layout (e.g., StackPane)
+        StackPane root = new StackPane();
+
+        // Add components or content to the layout
+        Label label = new Label("Hello, JavaFX!");
+        root.getChildren().add(label);
+
+        // Create the scene with layout
+        Scene scene = new Scene(root, 1024, 768);
+
+        // Set the scene on the primary stage
+        primaryStage.setScene(scene);
+
+        // Display the window
+        primaryStage.show();
+    }
 }
