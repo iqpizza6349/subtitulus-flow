89f0afeddf581f8524cba606f477a7ee8101bb5b
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-28 14:37:21 +0900

    :memo: :: add package-info.java for show memo in javadocs

---
+++ src/main/java/io/iqpizza6349/subflow/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This module is main module to launch the application
+ * <p>
+ *     No classes in here except main class, packages only
+ * </p>
+ *
+ * @since 0.1.0
+ * @author iqpizza6349
+ * @version 0.1.0
+ */
+package io.iqpizza6349.subflow;
