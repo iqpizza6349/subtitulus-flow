359042777201324819fbb7382ad3c067f5e6cce0
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-28 14:38:31 +0900

    :memo: :: add package-info.java in window.menu package for javadoc

---
+++ src/main/java/io/iqpizza6349/subflow/window/menu/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This module is defines about menu components
+ * <p>
+ *     Do not define menubar class that use on scene or pane
+ * </p>
+ *
+ * @since 0.1.0
+ * @author iqpizza6349
+ * @version 0.1.0
+ */
+package io.iqpizza6349.subflow.window.menu;
