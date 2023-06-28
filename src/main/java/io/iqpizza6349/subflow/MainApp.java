b452031dcf79c7632d311219613f817a4bf04690
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-28 14:35:56 +0900

    :hammer: :: add plugin 'application' and update application info in build.gradle

--- src/main/java/io/iqpizza6349/subflow/MainApp.java
+++ src/main/java/io/iqpizza6349/subflow/MainApp.java
@@ -1,9 +1,14 @@
-package io.iqpizza6349.subflow;
+package io.iqpizza.subflow;
 
+import io.iqpizza.subflow.window.menu.WindowMenu;
+import io.iqpizza.subflow.window.menu.MenuType;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -18,6 +23,16 @@ public class MainApp extends Application {
         // Add components or content to the layout
         Label label = new Label("Hello, JavaFX!");
         root.getChildren().add(label);
+
+        // Add menu, and menu items to the menubar
+        Menu fileMenu = new WindowMenu(MenuType.FILE);
+        Menu editMenu = new WindowMenu(MenuType.EDIT);
+        Menu runMenu = new WindowMenu(MenuType.RUN);
+        Menu helpMenu = new WindowMenu(MenuType.HELP);
+
+        MenuBar menuBar = new MenuBar(fileMenu, editMenu, runMenu, helpMenu);
+        VBox vBox = new VBox(menuBar);
+        root.getChildren().add(vBox);
 
         // Create the scene with layout
         Scene scene = new Scene(root, 1024, 768);
