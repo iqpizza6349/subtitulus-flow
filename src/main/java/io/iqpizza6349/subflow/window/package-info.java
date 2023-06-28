50ae7df62d84f736d834ab7316abb5190d5877eb
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-28 14:40:47 +0900

    :memo: :: add package-info.java for window scene(form) in javadoc

---
+++ src/main/java/io/iqpizza6349/subflow/window/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This module is define that used for window components
+ * <p>
+ *     usually define or create javafx scene wrapper classes
+ *     such as MenuBar, Button
+ * </p>
+ *
+ * @since 0.1.0
+ * @author iqpizza6349
+ * @version 0.1.0
+ */
+package io.iqpizza6349.subflow.window;
