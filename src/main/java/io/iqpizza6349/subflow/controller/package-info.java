a713dea0be9ea4b364e18e515c2ae543143424ea
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-29 16:51:04 +0900

    :memo: :: add package-info.java for controller package(comment)

---
+++ src/main/java/io/iqpizza6349/subflow/controller/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package is defines about controller classes that used on fxml
+ * component controls
+ *
+ * @since 0.1.0
+ * @author iqpizza6349
+ * @version 0.1.0
+ */
+package io.iqpizza6349.subflow.controller;
