c5ecbc8956820cd1f9f5e3e12f4c90224f74a82f
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-29 10:01:03 +0900

    :memo: :: update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,12 +3,7 @@ module io.iqpizza6349.subflow {
     requires javafx.controls;
 
     requires org.controlsfx.controls;
-//    requires com.dlsc.formsfx;
-//    requires net.synedra.validatorfx;
-//    requires org.kordamp.ikonli.javafx;
-//    requires org.kordamp.bootstrapfx.core;
-//    requires eu.hansolo.tilesfx;
 
-    opens io.iqpizza6349.subflow to javafx.fxml;
+    opens io.iqpizza6349.subflow.controller to javafx.fxml;
     exports io.iqpizza6349.subflow;
 }
