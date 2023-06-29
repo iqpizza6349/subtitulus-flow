6ee459fe247c0d41fb46f5510fff5df07b883ece
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-29 16:53:19 +0900

    :memo: :: add package-info.java for interaction modules(comment)

---
+++ src/main/java/io/iqpizza6349/subflow/interaction/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package is defines the interaction classes using in controllers
+ * <p>
+ *    Defines interaction modules like button interaction, video preview,
+ *    short-cuts. The classes in this package are have responsibility of reply.
+ * </p>
+ * @since 0.1.0
+ * @author iqpizza6349
+ * @version 0.1.0
+ */
+package io.iqpizza6349.subflow.interaction;
