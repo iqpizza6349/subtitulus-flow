6f9b15a7d348868b759ac95dceb284614d75aee1
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-29 16:48:51 +0900

    :bento: :: add log4j dependencies in module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,9 @@ module io.iqpizza6349.subflow {
 
     requires org.controlsfx.controls;
 
+    requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
+
     opens io.iqpizza6349.subflow.controller to javafx.fxml;
     exports io.iqpizza6349.subflow;
 }
