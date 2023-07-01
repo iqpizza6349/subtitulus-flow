eb9a197c741deb9bf518c828fe519fb3e361f54a
iqpizza6349 <iqpizza6349@gmail.com>
2023-07-01 11:18:57 +0900

    :construction: :: add jcodec module in subtitulusf!ow project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,10 @@ module io.iqpizza6349.subflow {
     requires org.apache.logging.log4j;
     requires org.apache.logging.log4j.core;
 
+    requires jcodec;
+    requires java.desktop;
+
     opens io.iqpizza6349.subflow.controller to javafx.fxml;
+    opens io.iqpizza6349.subflow to jcodec;
     exports io.iqpizza6349.subflow;
 }
